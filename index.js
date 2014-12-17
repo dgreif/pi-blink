1830ae460300a5ca6c928378760dfd91f3520238
Dusty Greif <dustin.greif@gmail.com>
2014-12-17 03:30:29 +0000

    Add server with websockets

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
+var express = require('express');
+var app = express();
+var socketio = require('socket.io');
+var server = app.listen(8080);
+var io = socketio.listen(server);
 var gpio = require("pi-gpio");
+var indexFile = require('path').join(__dirname,'public','index.html');
+var isOn = false;
 
-gpio.open(7, "output", function(err) {        // Open pin 16 for output
-    gpio.write(7, 1, function() {            // Set pin 16 high (1)
-	setTimeout(function() {
-	    gpio.write(7, 0, function() {
-	        gpio.close(7);                        // Close pin 16
-	    });
-	}, 5000);
+app.get('/',function(req,res){
+    res.sendFile(indexFile);
+});
+
+app.post('/on', turnOn);
+
+io.on('connection',function(client){
+    client.emit(isOn ? 'on' : 'off');
+
+    client.on('on',function(data){
+        turnOn();
+        client.broadcast.emit('on',data);
+    });
+    client.on('off',function(data){
+        turnOff();
+        client.broadcast.emit('off',data);
+    });
+
+    client.on('disconnect',function(){
+        //client is gone.
     });
 });
 
-module.exports = {};
+gpio.close(7, function() {
+    gpio.open(7, "output", function() {
+        turnOn();
+    });
+});
+
+
+function turnOff () {
+    isOn = false;
+    gpio.write(7, 0);
+}
+
+function turnOn () {
+    isOn = true;
+    gpio.write(7, 1);
+}
